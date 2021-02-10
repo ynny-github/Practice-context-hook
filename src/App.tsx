3b18fdd5468677fe4a8e91053d9bfd52eee7a8bc
Yuya Nagai <ynny.opem@gmail.com>
2021-02-10 11:44:18 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const testContext = createContext("test-string");
+const contextWithDic = createContext({
+  "count": -1,
+  "handleClick": () => {}
+})
+
+function TestDisplay() {
+  const text = useContext(testContext)
+  return(
+    <React.Fragment>
+      <p>{text}</p>
+      <testContext.Consumer>
+        {value => <p>{value}</p>}
+      </testContext.Consumer>
+    </React.Fragment>
+  )
+}
+
+function TestBotton() {
+  const {count, handleClick} = useContext(contextWithDic)
+  return(
+    <React.Fragment>
+      <button onClick={handleClick}>count up</button>
+      <p>{count}</p>
+      <contextWithDic.Consumer>
+        {value => 
+        <React.Fragment>
+        <button onClick={value.handleClick}>count up</button>
+        <p>{value.count}</p>
+        </React.Fragment>
+      }
+      </contextWithDic.Consumer>
+    </React.Fragment>
+  )
+}
+
 function App() {
+  const [ count, setCount ] = useState(0);
+  const handleClick = () => {
+    setCount(count+1)
+  }
+  const test = "replace string"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* プロバイダーあり */}
+      <testContext.Provider value={test}>
+        <TestDisplay />
+      </testContext.Provider>
+      <contextWithDic.Provider value={{count, handleClick}} >
+        <TestBotton />
+      </contextWithDic.Provider>
+      {/* プロバイダーなし */}
+      <TestDisplay />
+      <TestBotton />
     </div>
   );
 }
